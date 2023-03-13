1b245582af14f8342a8e18d2f644d51b907c6bf7
Tim Miller <tim_j_miller@yahoo.com>
2023-03-12 20:11:28 -0400

    Added color and a check for a win

--- tic-tac-go/MainWindow.xaml.cs
+++ tic-tac-go/MainWindow.xaml.cs
@@ -30,6 +30,7 @@ namespace tic_tac_go
         List<string> listA = new List<string>();
         List<string> listB = new List<string>();
         TextBlock selection;
+        bool game_over = false;
         private void Grid_Loaded(object sender, RoutedEventArgs e)
         {
             reset();
@@ -39,7 +40,7 @@ namespace tic_tac_go
         {
             var mouseWasDownOn = e.Source as FrameworkElement;
 
-            if (txt_challenge.Text.Equals(""))
+            if (txt_challenge.Text.Equals("") && !game_over)
                 if (mouseWasDownOn != null)
                 {
                     if (mouseWasDownOn.Tag != null)
@@ -54,11 +55,17 @@ namespace tic_tac_go
 
         private void btn_X_Click(object sender, RoutedEventArgs e)
         {
-            if (txt_challenge.Text != "")
+            if (txt_challenge.Text != "" && !game_over)
             {
                 selection.Text = "X";
                 selection.TextDecorations = TextDecorations.Underline;
+                selection.Foreground = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FFE4572E"));
                 txt_challenge.Text = "";
+                game_over = checkForWin();
+            }
+            if (game_over)
+            {
+                txt_challenge.Text = "X Wins";
             }
         }
 
@@ -69,20 +76,87 @@ namespace tic_tac_go
 
         private void btn_O_Click(object sender, RoutedEventArgs e)
         {
-            if (txt_challenge.Text != "")
+            if (txt_challenge.Text != "" && !game_over)
             {
                 selection.Text = "O";
                 selection.TextDecorations = TextDecorations.Underline;
+                selection.Foreground = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FFF6AE2D"));
                 txt_challenge.Text = "";
+                game_over = checkForWin();
+            }
+            if (game_over)
+            {
+                txt_challenge.Text = "O Wins";
             }
         }
         private bool checkForWin()
         {
-            return false;
+            var c = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FF00A896"));
+            if (txt_0_0.Text == txt_0_1.Text && txt_0_1.Text == txt_0_2.Text && !txt_0_0.Text.Equals("_"))
+            {
+                txt_0_0.Background = c;
+                txt_0_1.Background = c;
+                txt_0_2.Background = c;
+                return true;
+            }
+            if (txt_1_0.Text == txt_1_1.Text && txt_1_1.Text == txt_1_2.Text && !txt_1_0.Text.Equals("_"))
+            {
+                txt_1_0.Background = c;
+                txt_1_1.Background = c;
+                txt_1_2.Background = c;
+                return true;
+            }
+            if (txt_2_0.Text == txt_2_1.Text && txt_2_1.Text == txt_2_2.Text && !txt_2_0.Text.Equals("_"))
+            {
+                txt_2_0.Background = c;
+                txt_2_1.Background = c;
+                txt_2_2.Background = c;
+                return true;
+            }
+            if (txt_0_0.Text == txt_1_0.Text && txt_1_0.Text == txt_2_0.Text && !txt_0_0.Text.Equals("_"))
+            {
+                txt_0_0.Background = c;
+                txt_1_0.Background = c;
+                txt_2_0.Background = c;
+                return true;
+            }
+            if (txt_0_1.Text == txt_1_1.Text && txt_1_1.Text == txt_2_1.Text && !txt_0_1.Text.Equals("_"))
+            {
+                txt_0_1.Background = c;
+                txt_1_1.Background = c;
+                txt_2_1.Background = c;
+                return true;
+            }
+            if (txt_0_2.Text == txt_1_2.Text && txt_1_2.Text == txt_2_2.Text && !txt_0_2.Text.Equals("_"))
+            {
+                txt_0_2.Background = c;
+                txt_1_2.Background = c;
+                txt_2_2.Background = c;
+                return true;
+            }
+            if (txt_0_0.Text == txt_1_1.Text && txt_1_1.Text == txt_2_2.Text && !txt_0_0.Text.Equals("_"))
+            {
+                txt_0_0.Background = c;
+                txt_1_1.Background = c;
+                txt_2_2.Background = c;
+                return true;
+            }
+            if (txt_0_2.Text == txt_1_1.Text && txt_1_1.Text == txt_2_0.Text && !txt_0_2.Text.Equals("_"))
+            {
+                txt_0_2.Background = c;
+                txt_1_1.Background = c;
+                txt_2_0.Background = c;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
         private void reset()
         {
             var rnd = new Random();
+            var c = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FFFF4343"));
             using (var reader = new StreamReader(@"challenges.csv"))
             {
                 while (!reader.EndOfStream)
@@ -110,7 +184,25 @@ namespace tic_tac_go
             txt_2_0.TextDecorations = null;
             txt_2_1.TextDecorations = null;
             txt_2_2.TextDecorations = null;
-
+            txt_0_0.Foreground = c;
+            txt_0_1.Foreground = c;
+            txt_0_2.Foreground = c;
+            txt_1_0.Foreground = c;
+            txt_1_1.Foreground = c;
+            txt_1_2.Foreground = c;
+            txt_2_0.Foreground = c;
+            txt_2_1.Foreground = c;
+            txt_2_2.Foreground = c;
+            txt_0_0.Background = null;
+            txt_0_1.Background = null;
+            txt_0_2.Background = null;
+            txt_1_0.Background = null;
+            txt_1_1.Background = null;
+            txt_1_2.Background = null;
+            txt_2_0.Background = null;
+            txt_2_1.Background = null;
+            txt_2_2.Background = null;
+            game_over = false;
         }
 
         private void btn_Reset_Click(object sender, RoutedEventArgs e)
